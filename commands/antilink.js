3ab6565c28ebc73b98f3fd043f0859b007e3a455
Zyphor Security <ceoilovemoneyorglideranca@gmail.com>
2026-05-15 18:29:44 -0300

    Implement anti-link command toggle

--- commands/antilink.js
+++ commands/antilink.js
@@ -1,1 +1,17 @@
+const { SlashCommandBuilder } = require("discord.js");
 
+let enabled = false;
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("antilink")
+    .setDescription("Ativar/desativar anti-link"),
+
+  async execute(interaction) {
+    enabled = !enabled;
+
+    interaction.reply(
+      `🔗 Anti-link está: **${enabled ? "ON" : "OFF"}**`
+    );
+  }
+};
