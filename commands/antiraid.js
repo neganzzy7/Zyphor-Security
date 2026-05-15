c282fd6d2c7d095d6bb472d7d294193c57c37e76
Zyphor Security <ceoilovemoneyorglideranca@gmail.com>
2026-05-15 18:30:05 -0300

    Add antiraid command to toggle anti-raid feature

--- commands/antiraid.js
+++ commands/antiraid.js
@@ -1,1 +1,17 @@
+const { SlashCommandBuilder } = require("discord.js");
 
+let enabled = false;
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("antiraid")
+    .setDescription("Ativar/desativar anti-raid"),
+
+  async execute(interaction) {
+    enabled = !enabled;
+
+    interaction.reply(
+      `🛡️ Anti-raid está: **${enabled ? "ON" : "OFF"}**`
+    );
+  }
+};
