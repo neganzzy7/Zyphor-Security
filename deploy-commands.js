36b846658653cf7fd3b316ae040afc8c2ebe996f
Zyphor Security <ceoilovemoneyorglideranca@gmail.com>
2026-05-15 15:55:46 -0300

    Add script to clear old Discord commands
    This script clears old commands from the Discord application using the REST API.

---
+++ deploy-commands.js
@@ -0,0 +1,18 @@
+const { REST, Routes } = require("discord.js");
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log("Limpando comandos antigos...");
+
+    await rest.put(
+      Routes.applicationCommands("SEU_CLIENT_ID"),
+      { body: [1501444873998303344] }
+    );
+
+    console.log("Comandos apagados com sucesso!");
+  } catch (error) {
+    console.error(error);
+  }
+})();
