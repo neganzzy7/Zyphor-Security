50f8ea4dc2d90cc252c20f7ea0fcb31a6acae893
Zyphor Security <ceoilovemoneyorglideranca@gmail.com>
2026-05-17 19:25:07 -0300

    Add ping command with latency response
    Implement ping command to check bot latency.

--- commands/ping.js
+++ commands/ping.js
@@ -1,1 +1,24 @@
+const {
+ SlashCommandBuilder
+} = require("discord.js");
 
+module.exports = {
+
+ data:
+ new SlashCommandBuilder()
+
+ .setName("ping")
+
+ .setDescription(
+  "Ver latência do bot"
+ ),
+
+ async execute(interaction) {
+
+  await interaction.reply(
+   "🏓 Pong!"
+  );
+
+ }
+
+};
