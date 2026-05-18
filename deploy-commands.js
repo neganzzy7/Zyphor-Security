ea5c1bdd9ea28a956cf06f7726276c152526db3c
Zyphor Security <ceoilovemoneyorglideranca@gmail.com>
2026-05-18 03:47:48 -0300

    Refactor command loading and registration process

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,29 +3,31 @@ const fs = require("fs");
 
 const commands = [];
 
-const commandFiles = fs.readdirSync("./commands").filter(f => f.endsWith(".js"));
+const files = fs.readdirSync("./commands").filter(f => f.endsWith(".js"));
 
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  
-  // proteção pra evitar quebrar
-  if (!command.data) continue;
+for (const file of files) {
+  const cmd = require(`./commands/${file}`);
 
-  commands.push(command.data.toJSON());
+  if (!cmd.data) {
+    console.log(`IGNORADO: ${file}`);
+    continue;
+  }
+
+  commands.push(cmd.data.toJSON());
 }
 
 const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 (async () => {
   try {
-    console.log(`Carregando ${commands.length} comandos...`);
+    console.log(`Registrando ${commands.length} comandos...`);
 
     await rest.put(
       Routes.applicationCommands(process.env.CLIENT_ID),
       { body: commands }
     );
 
-    console.log("✔ Slash commands atualizados!");
+    console.log("✔ Comandos registrados!");
   } catch (err) {
     console.error(err);
   }
