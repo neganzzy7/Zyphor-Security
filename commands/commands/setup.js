9e54208a5d4c53ba213a80b99ff7dbbd22edc362
Zyphor Security <ceoilovemoneyorglideranca@gmail.com>
2026-05-18 00:19:24 -0300

    Refactor setup command to use object literal

--- commands/commands/setup.js
+++ commands/commands/setup.js
@@ -1,11 +1,11 @@
-const { SlashCommandBuilder } = require("discord.js");
-
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("setup")
-    .setDescription("Setup do servidor"),
+  data: {
+    name: "setup",
+    description: "Setup do servidor",
+  },
 
   async execute(interaction) {
+    console.log("SETUP RODOU");
     await interaction.reply("✅ Setup funcionando!");
   },
 };
