46ae69d19cae864c16295c257bf0350902c1cc96
Zyphor Security <ceoilovemoneyorglideranca@gmail.com>
2026-05-18 02:22:20 -0300

    Add setup command for server initialization

--- commands/commands/setup.js
+++ commands/commands/setup.js
@@ -1,1 +1,11 @@
+const { SlashCommandBuilder } = require("discord.js");
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("setup")
+    .setDescription("Setup do servidor"),
+
+  async execute(interaction) {
+    await interaction.reply("✅ Setup funcionando!");
+  },
+};
