d25c3c2c68c64dd3faf2ea1d1165c1801884233a
Zyphor Security <ceoilovemoneyorglideranca@gmail.com>
2026-05-18 01:58:15 -0300

    Remove setup command implementation

--- commands/commands/setup.js
+++ commands/commands/setup.js
@@ -1,11 +1,1 @@
-const { SlashCommandBuilder } = require("discord.js");
 
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("setup")
-    .setDescription("Configuração do servidor"),
-
-  async execute(interaction) {
-    await interaction.reply("✅ Setup funcionando!");
-  },
-};
