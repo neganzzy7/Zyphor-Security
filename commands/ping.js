da10aac7e00653cfc2b2f3cd9e0ca1a29f9f865f
Zyphor Security <ceoilovemoneyorglideranca@gmail.com>
2026-05-18 00:45:00 -0300

    Refactor ping command structure and description
    Refactor ping command to streamline code and update description.

--- commands/ping.js
+++ commands/ping.js
@@ -1,24 +1,11 @@
-const {
- SlashCommandBuilder
-} = require("discord.js");
+const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Testa o bot"),
 
- data:
- new SlashCommandBuilder()
-
- .setName("ping")
-
- .setDescription(
-  "Ver latência do bot"
- ),
-
- async execute(interaction) {
-
-  await interaction.reply(
-   "🏓 Pong!"
-  );
-
- }
-
+  async execute(interaction) {
+    await interaction.reply("🏓 Pong!");
+  },
 };
