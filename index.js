c424a3460ae368cecf65cf6406035161b3fc498a
Zyphor Security <ceoilovemoneyorglideranca@gmail.com>
2026-05-18 02:00:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const { Client, GatewayIntentBits, Events } = require("discord.js");
 
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds],
+});
+
+client.once(Events.ClientReady, c => {
+  console.log(`✅ BOT ONLINE: ${c.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
