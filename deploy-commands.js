5a26682e30af07734fa6be80eea7f84669694784
Zyphor Security <ceoilovemoneyorglideranca@gmail.com>
2026-05-18 03:16:01 -0300

    Update logging messages for command registration

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,10 +2,15 @@ const { REST, Routes } = require("discord.js");
 const fs = require("fs");
 
 const commands = [];
+
 const commandFiles = fs.readdirSync("./commands").filter(f => f.endsWith(".js"));
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
+  
+  // proteção pra evitar quebrar
+  if (!command.data) continue;
+
   commands.push(command.data.toJSON());
 }
 
@@ -13,14 +18,14 @@ const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 (async () => {
   try {
-    console.log("Registrando slash commands...");
+    console.log(`Carregando ${commands.length} comandos...`);
 
     await rest.put(
       Routes.applicationCommands(process.env.CLIENT_ID),
       { body: commands }
     );
 
-    console.log("Slash commands registrados!");
+    console.log("✔ Slash commands atualizados!");
   } catch (err) {
     console.error(err);
   }
